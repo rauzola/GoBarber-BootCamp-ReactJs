fe4a1a8f4e4860a281f30e43ebfa69ca9f83449b
rauzola <raul_sigoli@hotmail.com>
2020-04-06 00:17:32 -0300

    06   Estilizando página Main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import { FaGithubAlt, FaPlus } from 'react-icons/fa';
 
-import { Title } from './styles';
+import { Container, From, SubmitButton } from './styles';
 
 export default function main() {
-    return <Title>Hello WORLD</Title>;
+    return (
+        <Container>
+            <h1>
+                <FaGithubAlt />
+                Repositórios
+            </h1>
+
+            <From onSubmit={() => {}}>
+                <input type="text" placeholder="Adicionar repositório" />
+
+                <SubmitButton disabled>
+                    <FaPlus color="#FFF" size={14} />
+                </SubmitButton>
+            </From>
+        </Container>
+    );
 }
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -1,5 +1,52 @@
 import styled from 'styled-components';
 
-export const Title = styled.h1`
-    color: #fff;
+export const Container = styled.div`
+    max-width: 700px;
+    background: #fff;
+
+    /*borda arrendodada */
+    border-radius: 40px 0px 40px 0px;
+
+    box-shadow: 0 0 20px rgba(0, 0, 0, 0.1);
+    padding: 30px;
+    margin: 80px auto;
+
+    h1 {
+        font-size: 20px;
+        display: flex;
+        flex-direction: row;
+        align-items: center;
+
+        svg {
+            margin-right: 10px;
+        }
+    }
+`;
+
+export const From = styled.form`
+    margin-top: 30px;
+    display: flex;
+    flex-direction: row;
+
+    input {
+        flex: 1;
+        border: 1px solid #eee;
+        padding: 10px 15px;
+        border-radius: 30px 0px 30px 0px;
+        font-size: 16px;
+    }
+`;
+
+export const SubmitButton = styled.button.attrs({
+    type: 'submit',
+})`
+    background: #7159c1;
+    border: 0;
+    padding: 0 15px;
+    margin-left: 10px;
+    border-radius: 10px 0px 10px 0px;
+
+    display: flex;
+    justify-content: center;
+    align-items: center;
 `;
